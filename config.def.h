9c898e28357d8e907ca8822df8288a5c57358f53
joe <rbo@gmx.us>
2025-10-15 16:30:33 +0200

    font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,8 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy  = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"BlexMono Nerd Font Medium:size=13"
+	"Terminess Nerd Font:style=SemiCondensed:size=15"
+	// "BlexMono Nerd Font Medium:size=13"
 	// "BigBlueTermPlus Nerd Font:size=13"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
