23e8294139ebd6bfd93dca6af69598934871b327
rudy.bousset <rudy.bousset@leviia.com>
2026-06-02 18:08:43 +0200

    new gruv

--- config.def.h
+++ config.def.h
@@ -12,10 +12,10 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#ebdbb2", "#1d2021" },
+	[SchemeNorm] = { "#ebdbb2", "#1b1b1b" },
 	[SchemeSel] = { "#ebdbb2", "#cc241d" },
 	[SchemeSelHighlight] = { "#fabd2f", "#cc241d" },
-	[SchemeNormHighlight] = { "#fabd2f", "#1d2021" },
+	[SchemeNormHighlight] = { "#fabd2f", "#1b1b1b" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
