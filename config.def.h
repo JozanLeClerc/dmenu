aca3ea24385cab4a6ef57d90bf9491e95c88073e
rudy.bousset <rudy.bousset@leviia.com>
2026-06-04 17:39:00 +0200

    ok

--- config.def.h
+++ config.def.h
@@ -8,7 +8,7 @@ static const char *fonts[] = {
 	// "MonaspiceNe Nerd Font:style=Medium:size=12"
 	// "BlexMono Nerd Font:style=Medium:size=13"
 	// "Terminess Nerd Font:size=15"
-	"Terminus (TTF):size=15"
+	"Terminus (TTF):size=13"
 	// "BigBlueTermPlus Nerd Font:size=13"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
