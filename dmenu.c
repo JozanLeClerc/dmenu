2576908608e56ab075680a4d3c67c8e654873095
rudy.bousset <rudy.bousset@leviia.com>
2026-07-24 16:01:20 +0200

    pretty cool

--- dmenu.c
+++ dmenu.c
@@ -871,7 +871,7 @@ setup(void)
 	swa.override_redirect = True;
 	swa.background_pixel = scheme[SchemeNorm][ColBg].pixel;
 	swa.event_mask = ExposureMask | KeyPressMask | VisibilityChangeMask;
-	win = XCreateWindow(dpy, root, x - border_width, y - border_width, mw, mh, border_width,
+	win = XCreateWindow(dpy, root, x, y - border_width, mw - (border_width * 2), mh, border_width,
 	                    CopyFromParent, CopyFromParent, CopyFromParent,
 	                    CWOverrideRedirect | CWBackPixel | CWEventMask, &swa);
 	if (border_width)
